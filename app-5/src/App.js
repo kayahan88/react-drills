930ad55f47a90aa871beb9b8d7b1c3191d8010a7
Kaya Stokes <kayatabitha@gmail.com>
2021-03-03 20:43:48 -0700

    app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Image from './Components/Image'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  class App extends Component {
+    render(){
+      return(
+        <div>
+          <Image url={'https://cdn-0.therandomvibez.com/wp-content/uploads/2017/06/Famous-SPongebob-Quote.jpg'} />
+        </div>
+      )
+    }
+  }
+  
+
+
+
+export default App
+
